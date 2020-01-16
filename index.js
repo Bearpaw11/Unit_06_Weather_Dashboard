545828a55bc060c52d3ae1e93cefe3710043e3b9
Chris Behrens <chrisbehrens@yahoo.com>
2020-01-15 19:53:48 -0700

    updated recent search JavaScript so it get the whole name of the city

--- index.js
+++ index.js
@@ -1,87 +1,89 @@
  const apiKey = "893c6340b3ebe569233d6eaa2bb7981b"
 
  $(document).ready(function() {
-  
-  var search = []
-  var storage = localStorage.getItem("city")
-  console.log(storage)
-  if (storage){
-    search = JSON.parse(storage)
-  }
-  renderside()
-  searchcity(search[search.length -1])
-  
+
+     var search = []
+     var storage = localStorage.getItem("city")
+     
+     if (storage) {
+         search = JSON.parse(storage)
+ 
+     }
+     renderside()
+     searchcity(search[search.length - 1])
 
 
 
-$(".getWeather").on("click", function(event) {
 
-        event.preventDefault();
-        var NowMoment = moment();
-        $(".currentDate").text(NowMoment.format("MMMM Do YYYY, h:mm a"));
-        
-        const cityWeather = $(".city").val();
-        console.log("--->", search.indexOf(cityWeather))
-   
-      
-        
-        searchcity(cityWeather)
-})
-function searchcity(cityWeather){
-        const queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
-        "q=" + cityWeather + ",Burundi&units=imperial&appid=" + apiKey;
-       
+     $(".getWeather").on("click", function(event) {
 
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        }).then(function(response) {
-       
+         event.preventDefault();
+         var NowMoment = moment();
+         $(".currentDate").text(NowMoment.format("MMMM Do YYYY, h:mm a"));
 
-          if(search.indexOf(cityWeather) === -1){
-            search.push(cityWeather)
-            renderside()
+         const cityWeather = $(".city").val();
+         console.log("--->", search.indexOf(cityWeather))
 
-            
-            localStorage.setItem("city", JSON.stringify(search))
-        }
-          $(".city").html("<h2>" + response.name + " Weather</h2>");
-          var iconcode = response.weather[0].icon;
-          var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
-          $('#wicon').attr('src', iconurl);
-          $(".temp").text("Temperature (F): " + response.main.temp);
-          $(".windSpeed").text("Wind Speed (MPH): " + response.wind.speed);
-          $(".humidity").text("Humidity (%): " + response.main.humidity);
-          var lat = response.coord.lat
-          var lon = response.coord.lon  
 
-          var uvIndexQuery= "https://api.openweathermap.org/data/2.5/uvi?&appid=893c6340b3ebe569233d6eaa2bb7981b&lat=" + lat + "&lon=" + lon 
-         
 
-          console.log(uvIndexQuery)
-          $.ajax({
-            url: uvIndexQuery,
-            method: "GET"
-          }).then(function(response) {
-            console.log (response)
-            $(".uvIndex").text("UV: " + response.value);
-          })
-        
+         searchcity(cityWeather)
+     })
 
-        });
-        var url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityWeather + '&appid=893c6340b3ebe569233d6eaa2bb7981b&units=imperial'
-        $.ajax({
-          url: url,
-          method: "GET"
-        }).then(function(response) {
-          $( "#forecast" ).empty();
-          console.log (response)
-          for (let i=0; i< response.list.length; i++){
-          if (response.list[i].dt_txt.indexOf("12:00:00") !== -1 ){
-            console.log(response.list[i])
-            var iconcode = response.list[i].weather[0].icon;
-            console.log(iconcode)
-           var card= `<div class="col-7 col-lg-2 all"><div class="card bg-primary text-white">
+     function searchcity(cityWeather) {
+         const queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+             "q=" + cityWeather + ",Burundi&units=imperial&appid=" + apiKey;
+
+
+         $.ajax({
+             url: queryURL,
+             method: "GET"
+         }).then(function(response) {
+
+
+             if (search.indexOf(cityWeather) === -1) {
+                 search.push(cityWeather)
+                 renderside()
+
+
+                 localStorage.setItem("city", JSON.stringify(search))  
+             }
+             $(".city").html("<h2>" + response.name + " Weather</h2>");
+             var iconcode = response.weather[0].icon;
+             var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+             $('#wicon').attr('src', iconurl);
+             $(".temp").text("Temperature (F): " + response.main.temp);
+             $(".windSpeed").text("Wind Speed (MPH): " + response.wind.speed);
+             $(".humidity").text("Humidity (%): " + response.main.humidity);
+             var lat = response.coord.lat
+             var lon = response.coord.lon
+
+             var uvIndexQuery = "https://api.openweathermap.org/data/2.5/uvi?&appid=893c6340b3ebe569233d6eaa2bb7981b&lat=" + lat + "&lon=" + lon
+
+
+             console.log(uvIndexQuery)
+             $.ajax({
+                 url: uvIndexQuery,
+                 method: "GET"
+             }).then(function(response) {
+                 console.log(response)
+                 $(".uvIndex").text("UV: " + response.value);
+             })
+
+
+         });
+         var url = 'https://api.openweathermap.org/data/2.5/forecast?q=' + cityWeather + '&appid=893c6340b3ebe569233d6eaa2bb7981b&units=imperial'
+         $.ajax({
+             url: url,
+             method: "GET"
+         }).then(function(response) {
+             $("#forecast").empty();
+             console.log(response)
+             for (let i = 0; i < response.list.length; i++) {
+                 if (response.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+                     console.log(response.list[i])
+                     var iconcode = response.list[i].weather[0].icon;
+                     console.log(iconcode)
+                     var card = `<div class="col-7 col-lg-2 all"><div class="card bg-primary text-white">
            <div class="card-body">
            <h5 class="card-title">${response.list[i].dt_txt.split(" ")[0]}</h5>
                 <img src=${"http://openweathermap.org/img/w/" + iconcode + ".png"} class="icon2" alt="...">
@@ -91,32 +93,49 @@ function searchcity(cityWeather){
               </div>
               </div>`
 
-              $('#forecast').append(card)
-          }
+                     $('#forecast').append(card)
+                 }
 
-          }
+             }
 
-          
-        })
 
-      };
+         })
 
-     
-function renderside(){
- 
-  let max = search.length > 5 ? 5 : search.length;
-  $("#list").empty()
-  let j=0
-  for (let i=search.length -1; j<max; i--){
-  $("#list").append(`<li class="list-group-item topcities" city=${search[i]}>${search[i]}</li>`)
-  j++
-  }
-  $(".topcities").on("click", function(){
-    let cityWeather = $(this).attr("city")
-    searchcity(cityWeather)
+     };
 
-  })
-}
 
-    });
+     function renderside() {
+
+         let max = search.length > 5 ? 5 : search.length;
+
+        //  var max;
+        //  if (search.length > 5){
+        //    max = 5
+        //  }
+        //  else{
+        //    max = search.length
+        //  }
+         $("#list").empty()
+         let j = 0
+         for (let i = search.length - 1; j < max; i--) {
+             $("#list").append(`<li class="list-group-item topcities" city=${i}>${search[i]}</li>`)
+             j++
+         }
+
+
+         $(".topcities").on("click", function() {
+             let index = $(this).attr("city")
+             console.log(index, search)
+             let cityWeather = search[index]
+             console.log(cityWeather)
+             searchcity(cityWeather)
+
+         })
+     }
+
+ });
+
+
+
+
 
